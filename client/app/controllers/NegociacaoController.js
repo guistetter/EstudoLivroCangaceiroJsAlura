b5aa08024c2f905db3e3e60a2146631cfb0a1cc3
guistetter <guilherme_stetter@hotmail.com>
2020-06-30 18:12:16 -0300

    pegando a data

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,5 +1,7 @@
 class NegociacaoController {
+
   constructor(){
+
     let $ = document.querySelector.bind(document);
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
@@ -8,21 +10,27 @@ class NegociacaoController {
 
   adiciona(event){
     event.preventDefault();
-
+    console.log("nova");
     //retorn string console.log(typeof(inputData.value));
     console.log(this._inputData.value);
     console.log(parseInt(this._inputQuantidade.value));
     console.log(parseFloat(this._inputValor.value));
     console.log(typeof(this._inputData.value))
 
-    let data = new Date(this._inputData.value.split('-'));
-    console.log(data)
+    //let data = new Date(this._inputData.value.split('-'));;
+    let data = new Date(...this._inputData.value.split('-')
+    .map((item, indice) => item - indice % 2)
+    );
+    console.log(data);
 
     let negociacao = new Negociacao(
-      this._inputData.value, 
+      data,
       parseInt(this._inputQuantidade.value),
       parseFloat(this._inputValor.value)
-    )
-    console.log(negociacao)
+    );
+
+    console.log(negociacao);
+    console.log("nova");
+    console.log("nova" + negociacao.data);
   }
 }
--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,27 +1,28 @@
-class Negociacao{
-  constructor(_data, _quantidade, _valor){
+class Negociacao {
 
-    Object.assign(this, {_quantidade, _valor});
-    this._data= new Date(_data.getTime()),
+  constructor(_data, _quantidade, _valor) {
+      Object.assign(this, { _quantidade, _valor })
+      this._data = new Date(_data.getTime());
+      Object.freeze(this);
+  }
 
-    // this._data = new Date(data.getTime());
-    // this._quantidade = quantidade;
-    // this._valor = valor;
+  get volume() {
 
-    Object.freeze(this);
+      return this._quantidade * this._valor;
   }
-  
-  get volume(){
-    return this._quantidade * this._valor;
+
+  get data() {
+
+      return new Date(this._data.getTime());
   }
-  get data(){
-    //return this._data;
-    return new Date(this._data.getTime())
+
+  get quantidade() {
+
+      return this._quantidade;
   }
-  get quantidade(){
-    return this._quantidade;
-  }
-  get valor(){
-    return this._valor;
+
+  get valor() {
+
+      return this._valor;
   }
 }
